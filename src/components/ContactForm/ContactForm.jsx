5d36c4aba46e3ae21f04a551acdeafca26a73b64
Oleksandr-N <hekp1984@gmail.com>
2023-03-31 17:18:49 +0300

    add validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 import s from './ContactForm.module.css';
 
@@ -13,9 +14,33 @@ function ContactForm({ onSubmitForm }) {
     number: '',
   };
 
+  const nameRegex = RegExp(
+    "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  );
+
+  const phoneRegex = RegExp(
+    /^[\+]?\d?[\s]?8?[\s]?\(?0\d{2}?\)?[\s]?\d{3}[\s|-]?\d{2}[\s|-]?\d{2}$/
+  );
+
+  const SignupSchema = Yup.object({
+    name: Yup.string(
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+      .matches(nameRegex, 'Invalid name')
+      .min(5, 'Too Short!')
+      .max(30, 'Too Long!')
+      .required('Required'),
+    number: Yup.string(
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+      .matches(phoneRegex, 'Invalid phone')
+      .required('Required'),
+  });
+
   return (
     <Formik
       initialValues={initialValues}
+      validationSchema={SignupSchema}
       onSubmit={handleFormSubmit}
       className={s.form}
     >
@@ -25,10 +50,12 @@ function ContactForm({ onSubmitForm }) {
           <Field
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
             className={s.input}
+            placeholder="Rosie Simpson"
+          />
+          <ErrorMessage
+            name="name"
+            render={msg => <div>{msg}</div>}
           />
         </label>
         <label className={s.label}>
@@ -36,10 +63,12 @@ function ContactForm({ onSubmitForm }) {
           <Field
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
             className={s.input}
+            placeholder="+380965432100"
+          />
+          <ErrorMessage
+            name="number"
+            render={msg => <div>{msg}</div>}
           />
         </label>
         <button type="submit" className={s.btn}>
